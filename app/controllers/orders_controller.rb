5bc0384584bc37a9e5fbb8756bd961e3fbf3552b
danielmoralesp <daniel_moralesp@hotmail.com>
2017-11-17 11:01:43 -0500

    Connecting to a Slow Payment Processor with Active Job

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,7 +34,8 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderMailer.received(@order).deliver_later
+        ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
+        #OrderMailer.received(@order).deliver_later
         format.html { redirect_to @order, notice: 'Thank you for your order.' }
         format.json { render :show, status: :created, location: @order }
       else
--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,5 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { args: ["--disable-infobars"] }
+  driven_by :selenium, using: :chrome, options: { args: ["--disable-infobars"] }
 end
